45b8a9b1f83febf69f965d4c5e60fe2c86aae4aa
Laflammex <92762772+GioAmira@users.noreply.github.com>
2022-04-05 16:49:46 -0400

    adding postgresql dependency

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -3,5 +3,7 @@ package com.revature;
 public class Driver {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("We are running a jar file...");
     }
 }
